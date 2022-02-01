83a267c73b6d31a05f57b5568b498c003d05b73b
Tegridy Code <marcgabel@live.dk>
2022-02-01 13:28:27 +0100

    Wipe

--- app/src/main/java/com/asyno/simpleproject/Female.kt
+++
@@ -1,9 +0,0 @@
-package com.asyno.simpleproject
-
-class Female {
-
-    public fun get(): Array<String> {
-        return arrayOf("Karoline", "Sarah", "Victoria", "Mikkeline");
-    }
-
-}
--- app/src/main/java/com/asyno/simpleproject/Male.kt
+++
@@ -1,9 +0,0 @@
-package com.asyno.simpleproject
-
-class Male {
-
-    public fun get(): Array<String> {
-        return arrayOf("Lars", "Peter", "Ole", "Jeppe");
-    }
-
-}
